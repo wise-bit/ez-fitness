bcc01a3dc6541f21bd174748d9fe726737c593da
Daniel Zhao <danielzhao2015@gmail.com>
2018-11-29 13:55:28 -0500

    main page hover mouse works

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,10 @@ public class Main{
     public static void main(String[] args) throws IOException {
 
         // new Login();
-        new Activity("Bench Press");
+        // new Activity("Bench Press");
+        new InfoScreen();
+
+
 
     }
 
